b90c2aef15450914c075c33a20edebd5d2f1b11f
Himanshu Singh <himanshusingh1k@gmail.com>
2026-06-22 21:26:47 +0530

    commit

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -1,12 +1,12 @@
 export default function Title({ text1, text2 }) {
   return (
     <div className="inline-flex gap-2 items-center mb-3">
-      <p className="text-gray-500">
+      <p className="text-gray-500 sm:text-xl">
         {text1}&nbsp;
-        <span className="text-gray-700 font-medium">{text2}</span>
+        <span className="text-gray-700 font-medium sm:text-xl">{text2}</span>
       </p>
       &nbsp;
-      <span className="w-12 h-0.5 bg-gray-600"></span>
+      <span className="w-12 h-0.5 bg-gray-600 hidden md:block"></span>
     </div>
   );
 }
